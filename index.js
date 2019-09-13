e681e4014e2d7b59ca2f4ab1accef2b01756b248
Eyal Toledano <eutait@gmail.com>
2019-09-13 17:39:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Your code here
+const justInvoke = function(fn) {
+  return fn.call(this);
+}
+
+const setThisWithCall = function(fn, thisValue, arg) {
+  return fn.call(thisValue, arg);
+};
+
+const setThisWithApply = function(fn, thisValue, args) {
+  return fn.apply(thisValue, args)
+}
+
+const returnNewFunctionOf = function (functionToBeCopied, thisValue) {
+  return functionToBeCopied.bind(thisValue)
+}
