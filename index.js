11fe69be354ba2696f6a0b6c86d229e6a636eeac
P0rtk3y <llama.p0rtk3y@gmail.com>
2019-09-20 22:23:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(fn){
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, argument){
+  return fn.call(thisValue, argument);
+}
+
+function setThisWithApply(fn, thisValue, argument){
+  return fn.apply(thisValue, argument);
+}
+
+function returnNewFunctionOf(copyFn, thisValue){
+  const newFn = copyFn.bind(thisValue);
+  return newFn;
+}
